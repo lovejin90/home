0fb4c59a1f24faf26677435a0a02ca8c9267d086
mdugi <falcon@kakao.com>
2024-08-05 00:22:55 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HotdealList from "./hotdeal/HotdealList";
-import Home from "./pages/Index";
+//import Home from "./pages/Index";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/list"  element={<HotdealList/>} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Home/>}/> */}
+        <Route path="/list" element={<HotdealList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
