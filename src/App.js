3740c9c4b4e3f8e24448088d0133efdc5b8c0c6c
lovejin90 <jun2642@gmail.com>
2024-08-05 15:00:42 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HotdealList from "./hotdeal/HotdealList";
-//import Home from "./pages/Index";
+import Main from "./pages/Index";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<Home/>}/> */}
+        <Route path="/" element={<Main/>}/>
         <Route path="/list" element={<HotdealList />} />
       </Routes>
     </BrowserRouter>
