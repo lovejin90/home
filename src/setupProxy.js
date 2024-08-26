9f4f3b60ee7c5bccda3f8c22833f75c5759dcd4a
mdugi <falcon@kakao.com>
2024-08-27 01:16:28 +0900

    test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,9 +6,6 @@ module.exports = function (app) {
     createProxyMiddleware({
       target: "http://ggoyo.com:8022/",
       changeOrigin: true,
-      pathRewrite: {
-        "^/api": "", // 하위 url 초기화
-      },
     })
   );
 
@@ -17,9 +14,6 @@ module.exports = function (app) {
     createProxyMiddleware({
       target: "http://apis.data.go.kr/B552555/lhLeaseNoticeInfo1",
       changeOrigin: true,
-      pathRewrite: {
-        "^/lh_api": "", // 하위 url 초기화
-      },
     })
   );
 };
