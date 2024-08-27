100d26d67dfa07662ee3c3ad7e3ae8c09204ed42
lovejin90 <jun2642@gmail.com>
2024-08-27 10:47:31 +0900

    test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://ggoyo.com:8022",
+      target: "https://api.ggoyo.com",
       changeOrigin: true,
     })
   );
